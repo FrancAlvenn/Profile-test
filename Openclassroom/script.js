c77b2a3dbff325f29715856ac6ea2100738e0949
Franc Alvenn Dela Cruz <102860848+FrancAlvenn@users.noreply.github.com>
2023-04-16 16:16:41 +0800

    Delete Openclassroom directory

--- Openclassroom/script.js
+++
@@ -1,9 +0,0 @@
-
-/*Drop Down*/
-function drop(){
-    if (document.querySelector(".dd-contents").style.display === "none"){
-        document.querySelector(".dd-contents").style.display = "inline";
-    } else {
-        document.querySelector(".dd-contents").style.display = "none";
-    }
-}
